5a00587e8e913c556eb034d5527047301f46db93
KenpachiZ11 <mr.daniil.b@mail.ru>
2021-01-14 20:51:24 +0300

    Carusel
    First work in js

---
+++ Carusel/main.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', () =>{
+
+    //buttons
+    const left = document.querySelector('.btn-left');
+    const right = document.querySelector('.btn-right');
+
+    //carusel
+
+    const slider = document.querySelector('.carusel__slide');
+    const images = document.querySelectorAll('.carusel__slide img');
+
+    //counter 
+
+    let counter = 0;
+    const stepSize = images[0].clientWidth;
+
+    slider.style.transform = 'translateX(' + `${-stepSize * counter}px`;
+
+
+    //btn
+
+    right.addEventListener('click', () => {
+        if(counter >= images.length - 1) counter = - 1;
+        slider.classList.add('transformAnimation')
+        counter++;
+        slider.style.transform = 'translateX(' + `${-stepSize * counter}px`;
+    });
+
+    left.addEventListener('click', () => {
+        if(counter <= 0) counter = images.length;
+        slider.classList.add('transformAnimation')
+        counter--;
+        slider.style.transform = 'translateX(' + `${-stepSize * counter}px`;
+    });
+
+
+});
